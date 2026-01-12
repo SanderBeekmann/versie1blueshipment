28af8cfe3c03c26fdee1aa54b20ca88f06a1f724
SanderBeekmann <sanderbeekman2003@gmail.com>
2026-01-12 15:02:32 +0100

    build fix

--- src/components/sections/TrustSection/TrustSection.js
+++ src/components/sections/TrustSection/TrustSection.js
@@ -57,13 +57,6 @@ const TrendingUpIcon = () => (
   </svg>
 );
 
-const ArrowRightIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M12 5L19 12L12 19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-);
-
 const CheckCircle2Icon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.7088 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1004 1.7649 16.2038 2.24013C18.3072 2.71537 20.2007 3.85781 21.6017 5.49706" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
